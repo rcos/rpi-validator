afd7dde7df985ebadfa1c8327fb951df958091c5
jakebillings <jake@jakebillings.com>
2018-10-26 15:57:07 -0400

    Fix: properly export all of the functions that are currently implemented

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+import isCRN from './isCRN';
 import isISO from './isISO';
 import isRIN from './isRIN';
+import isRPIEmail from './isRPIEmail';
 import isSubjectCode from './isSubjectCode';
 
 export default {
+    isCRN,
     isISO,
     isRIN,
-    isSubjectCode
+    isSubjectCode,
+    isRPIEmail
 };
